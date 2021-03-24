ad5137c12bc2ba604faf0c8704b921e37d03c651
Avinash Kumar <www.avinashkumar2@gmail.com>
2021-03-24 22:25:51 +0530

    api call method resolve

--- index.js
+++ index.js
@@ -19,10 +19,14 @@ app.post('/getResult',async (req, res) => {
     console.log(req.body)
     const rollNumebrs=req.body.rollNumbers.split(",")
     console.log(rollNumebrs)
-    const answer=await fetch("https://terriblytinytales.com/testapi?rollnumber=123");
-    // const json=await answer.json();
-    console.log(answer)
-    return res.send(answer);
+    let finalresult={};
+
+    rollNumebrs.forEach(async rol=>{
+        fetch("https://terriblytinytales.com/testapi?rollnumber="+rol);
+        finalresult[rol]=await answer.text();
+    })
+
+    return res.send("ok");
 })
 
 app.listen(process.env.PORT, function(req,result){
